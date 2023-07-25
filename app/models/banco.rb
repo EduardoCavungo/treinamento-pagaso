d90ce6edb8b5f06917a2531e0cc91a062d301ac6
Eduardo Cavungo <eeduardocavungo@gmail.com>
2023-07-25 19:25:05 +0100

    Refactorando a minha função de buscas utilizando CONCERNES

--- app/models/banco.rb
+++ app/models/banco.rb
@@ -1,12 +1,6 @@
 class Banco < ApplicationRecord
 
-  scope :by_nome, ->(nome) { where("lower(nome) like ?", "%#{nome.downcase}%") if nome.present? }
-  scope :by_email, ->(email) { where("lower(email) like ?", "%#{email.downcase}%") if email.present? }
-  scope :by_sigla, ->(sigla) { where("lower(sigla) like ?", "%#{sigla.downcase}%") if sigla.present? }
-
-  scope :by_morada, ->(morada) { where("lower(morada) like ?", "%#{morada.downcase}%") if morada.present? }
-  scope :by_telefone, ->(telefone) { where("lower(telefone) like ?", "%#{telefone.downcase}%") if telefone.present? }
-  scope :by_web_site, ->(web_site) { where("lower(web_site) like ?", "%#{web_site.downcase}%") if web_site.present? }
+include Bancos::Busca
 
   def self.busca_por_parametro(params = {})
     bancos = Banco.all
